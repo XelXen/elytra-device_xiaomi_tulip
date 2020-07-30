0394a499e50f4d245f80b230b319675073c399b6
Bharat Pawar <bpawar@codeaurora.org>
2020-07-30 20:18:30 +0530

    tulip: vibrator: Enable LED based vibrator
    Enable LED based vibrator for sdm660 targets.
    
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc

--- device.mk
+++ device.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # Wifi
 PRODUCT_PACKAGES += \
     TulipWifiOverlay
