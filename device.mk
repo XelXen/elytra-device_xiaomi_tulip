43dd0b24688a8da1326280559040cf8edda5a2b0
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-21 18:23:14 +0300

    tulip: Ship cgroups.json and task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for products
    launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Jasmine_sprout and Wayne were launched with Oreo (API level 26), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>
    Change-Id: I1d0ad2586b3fca99c935c5044c0e1de9f86609d3
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- device.mk
+++ device.mk
@@ -42,6 +42,11 @@ PRODUCT_COPY_FILES += \
 # Consumerir
 BOARD_HAVE_IR := true
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(DEVICE_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Display calibration
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/display/qdcm_calib_data_boe_ft8719_fhdplus_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_boe_ft8719_fhdplus_video_mode_dsi_panel.xml \
